a2976b758488a92c57f0a31524ddb50f85b7cb79
John de Robles <jrbderobles@gmail.com>
2022-09-12 17:24:47 +0800

    Templating engines: Handlebars implementation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 // Dependency modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
 
 // Custom modules
 const adminData = require('./routes/admin');
@@ -11,8 +12,11 @@ const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', 'views'); // explicit setting of where views are located; no need to set if in views folder (default)
+app.engine('hbs', engine({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+
+//app.set('view engine', 'pug');
+app.set('views', 'views'); // explicit setting of where views are located; no need to set if in views folder (default) for pug
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
