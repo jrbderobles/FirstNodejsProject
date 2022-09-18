d6b1942f003b35f9f8ea97eb337cf9ca103243d5
John de Robles <jrbderobles@gmail.com>
2022-09-19 01:05:19 +0800

    Implemented NoSQL

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render('shop/index', {
         productList: products,
@@ -15,7 +15,7 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render('shop/product-list', {
         productList: products,
@@ -31,19 +31,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
 
-  // Product.findAll({
-  //   where: {id: productId}
-  // })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
-
-  Product.findByPk(productId)
+  Product.findById(productId)
     .then(product => {
       res.render('shop/product-detail', {
         product: product,
@@ -56,9 +44,6 @@ exports.getProduct = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user.getCart()
-    .then(cart => {
-      return cart.getProducts();
-    })
     .then(cartProducts => {
       res.render('shop/cart', {
         path: '/cart',
@@ -71,32 +56,10 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-
-  req.user.getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts({where: {id: productId}});
-    })
-    .then(products => {
-      let product;
-      if (products.length > 0) {
-        product = products[0]
-      }
-
-      if (product) {
-        const oldQuantity = product.cartItem.quantity;
-        newQuantity += oldQuantity;
-        return product;
-      } else {
-        return Product.findByPk(productId);
-      }
-    })
+  
+  Product.findById(productId)
     .then(product => {
-      return fetchedCart.addProduct(product, {
-        through: {quantity: newQuantity}
-      });
+      return req.user.addToCart(product);
     })
     .then(() => res.redirect('/cart'))
     .catch(err => console.log(err));
@@ -105,51 +68,19 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
 
-  req.user.getCart()
-    .then(cart => {
-      return cart.getProducts({where: {id: productId}});
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
-    .then(result => {
-      res.redirect('/cart');
-    })
+  req.user.deleteItemFromCart(productId)
+    .then(() => res.redirect('/cart'))
     .catch(err => console.log(err));
 }
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user.getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      console.log(products);
-      return req.user.createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = {quantity: product.cartItem.quantity};
-              return product;
-            })
-          );
-        })
-        .catch(err => console.log(err));;
-    })
-    .then(result => {
-      return fetchedCart.setProducts(null);
-    })
-    .then(result => {
-      res.redirect('/orders');
-    })
+  req.user.addOrder()
+    // .then(() => res.redirect('/orders'))
     .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({include: ['products']})
+  req.user.getOrders()
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
