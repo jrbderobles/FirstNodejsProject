0dc763bfe24335cfb32779536fcb950a9de9d92b
John de Robles <jrbderobles@gmail.com>
2022-09-21 01:03:24 +0800

    Added input validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,85 @@
 // Dependency modules
 const express = require('express');
+const { check, body } = require('express-validator/check');
 
 // Custom modules
 const authController = require('../controllers/auth');
 
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, {req}) => {
+        // if (value === 'test@email.com') {
+        //   throw new Error('This email address is forbidden.');
+        // }
+        // return true;
+
+        return User
+          .findOne({
+            email: value
+          })
+          .then(userDocument => {
+            if (userDocument) {
+              return Promise.reject('Email already exists, please use a different one.');
+            }
+          });
+      })
+      .normalizeEmail(),
+    body(
+      'password',
+      'Please enter a password with only numbers and strings and at least 5 characters.'
+    )
+      .isLength({
+        min: 5
+      })
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, {req}) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+  ],
+  authController.postSignup
+);
+
 router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .normalizeEmail(),
+    body(
+      'password',
+      'Please enter a password with only numbers and strings and at least 5 characters.'
+    )
+      .isLength({
+        min: 5
+      })
+      .isAlphanumeric()
+      .trim()
+  ],
+  authController.postLogin
+);
+
 router.post('/logout', authController.postLogout);
+
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
+
 router.get('/reset/:token', authController.getNewPassword);
 router.post('/new-password', authController.postNewPassword);
 
