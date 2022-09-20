188c06a99cf103a528d4b419008a9d3929a587bd
John de Robles <jrbderobles@gmail.com>
2022-09-20 21:35:06 +0800

    Added more authentication

--- routes/auth.js
+++ routes/auth.js
@@ -11,5 +11,9 @@ router.post('/signup', authController.postSignup);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
+router.get('/reset', authController.getReset);
+router.post('/reset', authController.postReset);
+router.get('/reset/:token', authController.getNewPassword);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
