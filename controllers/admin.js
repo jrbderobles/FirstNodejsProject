263d815d42b06a279881a1f9f9855ea5e4f70ade
John de Robles <jrbderobles@gmail.com>
2022-09-21 21:56:19 +0800

    Implemented async request for delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -182,8 +182,8 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const productId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const productId = req.params.productId;
 
   Product
     .findById(productId)
@@ -196,10 +196,14 @@ exports.postDeleteProduct = (req, res, next) => {
           userId: req.user._id
         });
     })
-    .then(() => res.redirect('/admin/products'))
+    .then(() => {
+      res.status(200).json({
+        message: 'Success!'
+      });
+    })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({
+        message: 'Deleting product failed.'
+      });
     });
 }
