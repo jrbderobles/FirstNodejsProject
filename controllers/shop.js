2e6e85ef68c73e8ab1ab0fb8299992bccb4b8f22
John de Robles <jrbderobles@gmail.com>
2022-09-21 21:15:27 +0800

    Added pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,13 +7,33 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const User = require('../models/user');
 
+const ITEMS_PER_PAGE = 3;
+
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product
+    .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product
+        .find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         productList: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -24,12 +44,30 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product
+    .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product
+        .find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         productList: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
