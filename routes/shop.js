8b0bc395387f3e46025adac39d5668865c6b3409
John de Robles <jrbderobles@gmail.com>
2022-09-21 22:54:37 +0800

    Added checkout and payment using Stripe

--- routes/shop.js
+++ routes/shop.js
@@ -9,14 +9,20 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
+
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', isAuth, shopController.getCart);
 router.post('/cart', isAuth, shopController.postCart);
+
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
-router.post('/create-order', isAuth, shopController.postOrder);
+
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout/sucess', isAuth, shopController.getCheckoutSuccess);
+router.get('/checkout/cancel', isAuth, shopController.getCheckout);
+
 router.get('/orders', isAuth, shopController.getOrders);
 router.get('/orders/:orderId', isAuth, shopController.getInvoice);
-// router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
